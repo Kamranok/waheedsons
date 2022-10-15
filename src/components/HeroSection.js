1fc69b7a7ecf6c79f565bebd0119517f4e55273b
Kamran Muhammad <kamranmuhammad@Kamrans-MacBook-Pro.local>
2022-10-15 12:50:18 -0500

    tailwind layout with flexCon flexChd bg colors

---
+++ src/components/HeroSection.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import illustrationIntro from '../img/illustration-intro.svg'
+
+const HeroSection = () => {
+  return (
+    <>
+      <section className='bg-red-400'>
+        <main className='container bg-flexCon flex flex-col-reverse md:flex-row px-6 mx-auto mt-10 space-y-0 md:space-y-0'>
+          {/* Left Side */}
+          <div className='flex flex-col mb-32 space-y-12 md:w-1/2'>
+            <h1 className='max-w-md text-4xl font-bold text-center md:text-5xl md:text-left'>
+              Bring everyone together to build better product
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+              praesentium reiciendis optio, asperiores veniam est assumenda at,
+              labore vero doloribus nisi hic, corporis modi nemo saepe corrupti
+              sequi? Praesentium, natus?
+            </p>
+
+            {/* Button */}
+            <div className='flex justify-center md:justify-start'>
+              <a
+                href='/'
+                className='p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight'
+              >
+                Get Started
+              </a>
+            </div>
+          </div>
+          {/* Right Side */}
+          <div className='md:w/1/2'>
+            <img src={illustrationIntro} alt='hero' />
+          </div>
+        </main>
+      </section>
+    </>
+  )
+}
+
+export default HeroSection
---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import logo from '../img/logo.svg'
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className='relative container mx-auto p-6 '>
+        {/* Flex container */}
+        <div className='flex items-center justify-between bg-flexCon'>
+          {/* Logo */}
+          <div className='pt-2'>
+            <img src={logo} alt='logo' />
+          </div>
+          {/* Nenu Items */}
+          <div className='hidden md:flex space-x-6'>
+            <a href='/' className='hover:text-darkGrayishBlue'>
+              Pricing
+            </a>
+            <a href='/' className='hover:text-darkGrayishBlue'>
+              Product
+            </a>
+            <a href='/' className='hover:text-darkGrayishBlue'>
+              About Us
+            </a>
+            <a href='/' className='hover:text-darkGrayishBlue'>
+              Careers
+            </a>
+            <a href='/' className='hover:text-darkGrayishBlue'>
+              Community
+            </a>
+          </div>
+          {/* Button */}
+          <a
+            href='/'
+            className='hidden md:block p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight'
+          >
+            Get Started
+          </a>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
